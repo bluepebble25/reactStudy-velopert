136c0cb31a250df99d5b0778dbc35b3c70235c16
bluepebble25 <bluepebble25@gmail.com>
2023-05-02 16:20:19 +0900

    6-4) reducer 모듈 작성 및 store 적용

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import rootReducer from './modules';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
