30e1d95067108ba57597cb3bed0e69879f03a4a6
bluepebble25 <bluepebble25@gmail.com>
2023-05-18 21:35:01 +0900

    refactor: 7-6) 폴더 구조 개편 - 페이지를 생성하고 컨테이너를 페이지의 하위 요소로 이동

--- redux-middleware/src/App.js
+++ redux-middleware/src/App.js
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import CounterContainer from './components/CounterContainer';
-import PostListContainer from './components/PostListContainer';
+import PostListPage from './pages/PostListPage';
 
 function App() {
   return (
     <Routes>
-      <Route exact path="/" element={<PostListContainer />} />
+      <Route exact path="/" element={<PostListPage />} />
       <Route path="/counter" element={<CounterContainer />} />
     </Routes>
   );
